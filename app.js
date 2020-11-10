92fa22f35f323f9387d116ebc4ca15e2c8424dd4
Genos2000 <paraskamble2000@gmail.com>
2020-11-10 20:00:30 +0530

    test ccookies

--- app.js
+++ app.js
@@ -73,6 +73,7 @@ app.use(
         resave: false,
         saveUninitialized: true,
         store: sessionStore,
+        proxy : true,
         cookie: {
             maxAge: 1000 * 60 * 60 * 24, // Equals 1 day (1 day * 24 hr/1 day * 60 min/1 hr * 60 sec/1 min * 1000 ms / 1 sec)
         },
